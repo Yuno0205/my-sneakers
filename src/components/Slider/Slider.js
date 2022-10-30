05ce7cb32031686f75a17af1b256188190227c35
Yuno <mainhathao195@gmail.com>
2022-10-30 10:19:36 +0700

    UI Slider #2

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import clsx from 'clsx';
 function Slider() {
-    const [change, setChange] = useState(false);
+    const [change, setChange] = useState(true);
 
     const handleChange = (value) => {
         setChange(value);
@@ -14,59 +14,131 @@ function Slider() {
     };
     return (
         <div className={styles.wrapper}>
-            <div onClick={() => handleChange(false)} className={styles.prev}>
-                <div className={styles.prevIcon}>
-                    <PrevIcon />
-                </div>
-            </div>
-            <div onClick={() => handleChange(true)} className={styles.next}>
-                <div className={styles.nextIcon}>
-                    <NextIcon />
-                </div>
-            </div>
-            <div className={styles.sliderShow}>
-                <img src={images.slideBackground} />
-            </div>
-            <div className={styles.boxContent}>
-                <div className={styles.title}>
-                    <h3
-                        className={clsx(styles.title1, {
-                            [styles.active]: change,
-                        })}
-                    >
-                        Mordern
-                    </h3>
-                </div>
-                <div className={styles.title}>
-                    <h3
-                        className={clsx(styles.title2, {
-                            [styles.active]: change,
-                        })}
-                    >
-                        Sneaker
-                    </h3>
-                </div>
+            <div className={styles.cover}>
                 <div
-                    className={clsx(styles.price, {
-                        [styles.show]: change,
+                    className={clsx(styles.content, {
+                        [styles.current]: change,
                     })}
                 >
-                    {!change ? <p>from 122$</p> : <p>from 100$</p>}
+                    <div onClick={() => handleChange(false)} className={styles.prev}>
+                        <div className={styles.prevIcon}>
+                            <PrevIcon />
+                        </div>
+                    </div>
+                    <div onClick={() => handleChange(true)} className={styles.next}>
+                        <div className={styles.nextIcon}>
+                            <NextIcon />
+                        </div>
+                    </div>
+                    <div className={styles.slideInfo}>
+                        <div className={styles.sliderShow}>
+                            <img src={images.slideBackground} />
+                        </div>
+                        <div className={styles.boxContent}>
+                            <div className={styles.title}>
+                                <h3
+                                    className={clsx(styles.title1, {
+                                        [styles.active]: change,
+                                    })}
+                                >
+                                    Mordern
+                                </h3>
+                            </div>
+                            <div className={styles.title}>
+                                <h3
+                                    className={clsx(styles.title2, {
+                                        [styles.active]: change,
+                                    })}
+                                >
+                                    Sneaker
+                                </h3>
+                            </div>
+                            <div
+                                className={clsx(styles.price, {
+                                    [styles.show]: change,
+                                })}
+                            >
+                                <p>from 100$</p>
+                            </div>
+                            <div
+                                className={clsx(styles.boxBtn, {
+                                    [styles.show]: change,
+                                })}
+                            >
+                                <a>Shop now</a>
+                            </div>
+                        </div>
+                        <div
+                            className={clsx(styles.picture, {
+                                [styles.show]: change,
+                            })}
+                        >
+                            <img src={images.slide1} />
+                        </div>
+                    </div>
                 </div>
                 <div
-                    className={clsx(styles.boxBtn, {
-                        [styles.show]: change,
+                    className={clsx(styles.content, {
+                        [styles.current]: !change,
                     })}
                 >
-                    <a>Shop now</a>
+                    <div onClick={() => handleChange(false)} className={styles.prev}>
+                        <div className={styles.prevIcon}>
+                            <PrevIcon />
+                        </div>
+                    </div>
+                    <div onClick={() => handleChange(true)} className={styles.next}>
+                        <div className={styles.nextIcon}>
+                            <NextIcon />
+                        </div>
+                    </div>
+                    <div className={styles.slideInfo}>
+                        <div className={styles.sliderShow}>
+                            <img src={images.slideBackground} />
+                        </div>
+                        <div className={styles.boxContent}>
+                            <div className={styles.title}>
+                                <h3
+                                    className={clsx(styles.title1, {
+                                        [styles.active]: !change,
+                                    })}
+                                >
+                                    Collection
+                                </h3>
+                            </div>
+                            <div className={styles.title}>
+                                <h3
+                                    className={clsx(styles.title2, {
+                                        [styles.active]: !change,
+                                    })}
+                                >
+                                    Sneaker
+                                </h3>
+                            </div>
+                            <div
+                                className={clsx(styles.price, {
+                                    [styles.show]: !change,
+                                })}
+                            >
+                                <p>from 122$</p>
+                            </div>
+                            <div
+                                className={clsx(styles.boxBtn, {
+                                    [styles.show]: !change,
+                                })}
+                            >
+                                <a>Shop now</a>
+                            </div>
+                        </div>
+                        <div
+                            className={clsx(styles.picture, {
+                                [styles.show]: !change,
+                            })}
+                        >
+                            <img src={images.slide2} />
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div
-                className={clsx(styles.picture, {
-                    [styles.show]: change,
-                })}
-            >
-                {!change ? <img src={images.slide1} /> : <img src={images.slide2} />}
             </div>
         </div>
     );
